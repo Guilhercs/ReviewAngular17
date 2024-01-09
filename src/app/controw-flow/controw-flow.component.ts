aaa916ca1ad9f8709319a1a2ef6e1171b00dafde
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-01-09 20:15:42 -0300

    chore(control-flow): for

--- src/app/controw-flow/controw-flow.component.ts
+++ src/app/controw-flow/controw-flow.component.ts
@@ -1,18 +1,26 @@
+import { AsyncPipe, CommonModule,  } from '@angular/common';
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-controw-flow',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './controw-flow.component.html',
-  styleUrl: './controw-flow.component.scss'
+  styleUrl: './controw-flow.component.scss',
 })
 export class ControwFlowComponent {
-isTrue = false;
+  public isTrue = false;
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(4000));
+  public emptyArray$ = new BehaviorSubject<string[]>([])
 
-ngOnInit() {
-  setTimeout(() => {
-    this.isTrue = true;
-  }, 3000);
-}
+  ngOnInit() {
+    setTimeout(() => {
+      this.isTrue = true;
+    }, 3000);
+  }
 }
