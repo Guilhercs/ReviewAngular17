33edc42e4ba390665f4d9131e45580ab28e07f6a
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-01-13 21:27:27 -0300

    chore(environment): add homolog and dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-  <router-outlet></router-outlet>
-  `,
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {
-  title = 'EstudosAngular17';
+  constructor() {
+    console.info(environment.environment);
+  }
 }
